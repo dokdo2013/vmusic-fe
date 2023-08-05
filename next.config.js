fcdef0a4cfb238eb029d265559c81c793fd2e48e
hyeonwoojo <h_jo@ejn.gg>
2023-08-05 17:31:58 +0900

    feat: add env variable to next.confi.gjs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  // use process.env to get the value of the environment variable
+  env: {
+    // reference a value in the .env file
+    // note: the value is always a string (i.e. wrapped in quotes)
+    NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+  },
 };
 
 module.exports = nextConfig;
